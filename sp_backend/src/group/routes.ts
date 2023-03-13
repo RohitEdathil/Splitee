80c94f5f12cdefe440bd4b450d4da50e35bd49db
Rohit V <rohitedathil@gmail.com>
2023-03-13 13:37:37 +0530

    Follow up to prev

--- sp_backend/src/group/routes.ts
+++ sp_backend/src/group/routes.ts
@@ -1,17 +1,7 @@
 import { Router } from "express";
 import { validationMiddleware } from "../error/middlewares";
-import {
-  createGroupController,
-  editGroupController,
-  inviteUserController,
-  respondInviteController,
-} from "./controllers";
-import {
-  createGroupValidator,
-  editGroupValidator,
-  inviteUserValidator,
-  respondInviteValidator,
-} from "./validators";
+import { createGroupController, editGroupController } from "./controllers";
+import { createGroupValidator, editGroupValidator } from "./validators";
 
 const router = Router();
 
@@ -27,20 +17,6 @@ router.put(
   editGroupValidator,
   validationMiddleware,
   editGroupController
-);
-
-router.post(
-  "/invite",
-  inviteUserValidator,
-  validationMiddleware,
-  inviteUserController
-);
-
-router.post(
-  "/respond",
-  respondInviteValidator,
-  validationMiddleware,
-  respondInviteController
 );
 
 export default router;
