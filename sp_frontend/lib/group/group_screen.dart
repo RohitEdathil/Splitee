1ad926f07ac755cc4328ef19cae5bd94724c0ef3
Rohit V <rohitedathil@gmail.com>
2023-03-16 23:26:37 +0530

    Nav for group page ready

--- sp_frontend/lib/group/group_screen.dart
+++ sp_frontend/lib/group/group_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
+import 'package:sp_frontend/components/nav_bar_item.dart';
 import 'package:sp_frontend/group/group_provider.dart';
+import 'package:sp_frontend/home/components/nav_bar.dart';
 import 'package:sp_frontend/theme/colors.dart';
 
-class GroupScreen extends StatelessWidget {
+class GroupScreen extends StatefulWidget {
   final String groupId;
   const GroupScreen({super.key, required this.groupId});
 
   @override
+  State<GroupScreen> createState() => _GroupScreenState();
+}
+
+class _GroupScreenState extends State<GroupScreen> {
+  int active = 0;
+
+  final PageController controller = PageController();
+  int curPos = 0;
+
+  @override
   Widget build(BuildContext context) {
-    final group = context.watch<GroupProvider>().getGroup(groupId);
+    final group = context.watch<GroupProvider>().getGroup(widget.groupId);
 
     return group == null
         ? const Scaffold(
@@ -23,33 +35,54 @@ class GroupScreen extends StatelessWidget {
         : Scaffold(
             appBar: AppBar(
               foregroundColor: Palette.alpha,
-              actions: [
-                IconButton(
-                  onPressed: () {},
-                  tooltip: "Edit name",
-                  icon: const Icon(Icons.edit),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  tooltip: "Group QR code",
-                  icon: const Icon(Icons.qr_code_2),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  tooltip: "Leave group",
-                  icon: const Icon(Icons.logout_rounded),
-                ),
-              ],
+              scrolledUnderElevation: 0,
             ),
             body: SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.all(20),
                 child: Column(
                   children: [
-                    Center(
-                      child: Text(group.name,
-                          style: Theme.of(context).textTheme.headlineLarge),
+                    Text(group.name,
+                        style: Theme.of(context).textTheme.headlineLarge),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 60),
+                      child: Row(
+                        children: [
+                          NavBarItem(
+                              icon: Icons.shopping_cart,
+                              pos: 0,
+                              controller: controller,
+                              curPos: curPos),
+                          NavBarItem(
+                              icon: Icons.sync_alt_rounded,
+                              pos: 1,
+                              controller: controller,
+                              curPos: curPos),
+                          NavBarItem(
+                              icon: Icons.difference_outlined,
+                              pos: 2,
+                              controller: controller,
+                              curPos: curPos),
+                          NavBarItem(
+                              icon: Icons.settings,
+                              pos: 3,
+                              controller: controller,
+                              curPos: curPos),
+                        ],
+                      ),
                     ),
+                    const Divider(
+                      color: Palette.beta,
+                      thickness: 1,
+                    ),
+                    for (int i = 0; i < 10; i++)
+                      Container(
+                        height: 100,
+                        width: double.infinity,
+                        color: Palette.alpha,
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                      ),
                   ],
                 ),
               ),
--- sp_frontend/lib/home/components/nav_bar.dart
+++ sp_frontend/lib/home/components/nav_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sp_frontend/components/nav_bar_item.dart';
 import 'package:sp_frontend/theme/colors.dart';
 
 class HomeNavigation extends StatefulWidget {
@@ -59,71 +60,6 @@ class _HomeNavigationState extends State<HomeNavigation> {
           controller: widget.controller,
         ),
       ]),
-    );
-  }
-}
-
-class NavBarItem extends StatelessWidget {
-  final IconData icon;
-  final int pos;
-  final int curPos;
-  final PageController controller;
-  const NavBarItem(
-      {Key? key,
-      required this.icon,
-      required this.pos,
-      required this.controller,
-      required this.curPos})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () => controller.animateToPage(
-          pos,
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.ease,
-        ),
-        child: AnimatedScale(
-          scale: curPos == pos ? 1.1 : 1,
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.ease,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            color: Colors.transparent,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  icon,
-                  color: curPos == pos ? Palette.alpha : Palette.alphaDark,
-                ),
-                AnimatedScale(
-                  scale: curPos == pos ? 1 : 0,
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.ease,
-                  child: AnimatedPadding(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.ease,
-                    padding: EdgeInsets.all(
-                      curPos == pos ? 4 : 0,
-                    ),
-                    child: Container(
-                      height: 5,
-                      width: 5,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Palette.alpha,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
