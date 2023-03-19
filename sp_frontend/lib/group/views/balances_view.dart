309941b58555fa489fef190520a7931e6d1052db
Rohit V <rohitedathil@gmail.com>
2023-03-19 11:51:35 +0530

    UI improvements

--- sp_frontend/lib/group/views/balances_view.dart
+++ sp_frontend/lib/group/views/balances_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sp_frontend/components/white_padded_container.dart';
 import 'package:sp_frontend/group/components/balance_card.dart';
 import 'package:sp_frontend/group/components/view_common.dart';
 import 'package:sp_frontend/group/group_modal.dart';
@@ -12,7 +13,8 @@ class BalancesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewCommons(title: "Balances", children: [
       if (group.sortedBalances.isEmpty) const Empty(),
-      for (final balance in group.sortedBalances) BalanceCard(balance: balance)
+      for (final balance in group.sortedBalances)
+        WhitePaddedContainer(child: BalanceCard(balance: balance))
     ]);
   }
 }
--- sp_frontend/lib/group/views/payments_view.dart
+++ sp_frontend/lib/group/views/payments_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sp_frontend/components/white_padded_container.dart';
 import 'package:sp_frontend/group/components/payment_card.dart';
 import 'package:sp_frontend/group/components/view_common.dart';
 import 'package:sp_frontend/group/group_modal.dart';
@@ -14,7 +15,8 @@ class PaymentsView extends StatelessWidget {
       title: 'Summary',
       children: [
         if (group.payments.isEmpty) const Empty(),
-        for (final payment in group.payments) PaymentCard(payment: payment)
+        for (final payment in group.payments)
+          WhitePaddedContainer(child: PaymentCard(payment: payment))
       ],
     );
   }
